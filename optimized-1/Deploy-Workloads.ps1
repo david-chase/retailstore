72cb685d1630691115d4a6c0f47d6cd422937591
David Chase <dchase@hotmail.com>
2025-04-14 17:58:10 -0400

    Auto-commit by Git-Sync-Mandatory

--- optimized-1/Deploy-Workloads.ps1
+++ optimized-1/Deploy-Workloads.ps1
@@ -1,15 +1,15 @@
 Write-Host Deploying namespace unspecified -ForegroundColor Green
 k create ns unspecified
-k apply -f .\unspecified.yaml -n unspecified
+k apply -f unspecified.yaml -n unspecified
 Write-Host Deploying namespace loreq -ForegroundColor Green
 k create ns loreq
-k apply -f .\loreq.yaml -n loreq
+k apply -f loreq.yaml -n loreq
 Write-Host Deploying namespace lolim -ForegroundColor Green
 k create ns lolim
-k apply -f .\lolim.yaml -n lolim
+k apply -f lolim.yaml -n lolim
 Write-Host Deploying namespace hireq -ForegroundColor Green
 k create ns hireq
-k apply -f .\hireq.yaml -n hireq
+k apply -f hireq.yaml -n hireq
 Write-Host Deploying namespace hilim -ForegroundColor Green
 k create ns hilim
-k apply -f .\hilim.yaml -n hilim
+k apply -f hilim.yaml -n hilim
